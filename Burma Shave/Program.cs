c681588a1f3e86d29040b75e3425f2f99bdbd25e
Klonoa <klonoa3ds@gmail.com>
2015-09-21 11:26:42 -0400

    Commit Bleh
    blehhhhhhhh

---
+++ Burma Shave/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Burma_Shave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Shaving Brushes    ");
+            Console.WriteLine(" You'll soon see 'em");
+            Console.WriteLine(" On a shelf         ");
+            Console.WriteLine(" In some museum     ");
+            Console.WriteLine(" Bruma Shave        ");
+        }
+    }
+}
---
+++ Initials/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  JJJJJ     AAAAA  ");
+            Console.WriteLine("    J      A     A ");
+            Console.WriteLine("    J      AAAAAAA ");
+            Console.WriteLine("J   J      A     A ");
+            Console.WriteLine(" JJJ       A     A ");
+        }
+    }
+}
